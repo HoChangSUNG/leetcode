f1807df6b070fac8fe7e2656e81b484e0589af71
Ho <love47024702@naver.com>
2023-07-31 09:36:30 +0900

    7/31

---
+++ src/_7월/_21일_31일/_31일/SquaresOfASortedArray.java
@@ -0,0 +1,23 @@
+package _7월._21일_31일._31일;
+
+public class SquaresOfASortedArray {
+
+    public int[] sortedSquares(int[] nums) {
+        int i = 0;
+        int j = nums.length - 1;
+        int idx = nums.length - 1;
+
+        int[] result = new int[nums.length];
+        while (i <= j) {
+            if ((nums[i] * nums[i]) > (nums[j] * nums[j])) {
+                result[idx] = nums[i] * nums[i];
+                i++;
+            } else {
+                result[idx] = nums[j] * nums[j];
+                j--;
+            }
+            idx--;
+        }
+        return result;
+    }
+}
