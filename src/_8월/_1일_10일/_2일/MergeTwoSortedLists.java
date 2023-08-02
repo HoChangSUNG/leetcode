556372f07887ab99193c962c4beb7bc3a04cdec4
Ho <love47024702@naver.com>
2023-08-02 15:35:17 +0900

    8/2

---
+++ src/_8월/_1일_10일/_2일/MergeTwoSortedLists.java
@@ -0,0 +1,42 @@
+package _8월._1일_10일._2일;
+
+public class MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode node = new ListNode(1);
+        ListNode cur = node;
+        while (list1 != null || list2 != null) {
+            if (list1 != null && list2 != null) {
+                if (list1.val < list2.val) {
+                    cur.next = list1;
+                    list1 = list1.next;
+                } else {
+                    cur.next = list2;
+                    list2 = list2.next;
+                }
+            } else if (list1 != null) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        return node.next;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
