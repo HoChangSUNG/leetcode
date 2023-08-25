c92abaf869cd4a49dc0fee97ced6bdc81d9cd954
Ho <love47024702@naver.com>
2023-08-25 11:05:21 +0900

    8/25

---
+++ src/_8월/_21일_31일/_25일/DesignHashSet.java
@@ -0,0 +1,22 @@
+package _8월._21일_31일._25일;
+
+public class DesignHashSet {
+
+    private Integer[] arr;
+
+    public DesignHashSet() {
+        arr = new Integer[1000001];
+    }
+
+    public void put(int key, int value) {
+        arr[key] = value;
+    }
+
+    public int get(int key) {
+        return arr[key] == null ? -1 : arr[key];
+    }
+
+    public void remove(int key) {
+        arr[key] = null;
+    }
+}
