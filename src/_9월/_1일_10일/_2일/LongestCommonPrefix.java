4ed31b4dcd82782c68ca97817f0bce753c805805
Ho <love47024702@naver.com>
2023-09-02 14:20:57 +0900

    9/2

---
+++ src/_9월/_1일_10일/_2일/LongestCommonPrefix.java
@@ -0,0 +1,15 @@
+package _9월._1일_10일._2일;
+
+public class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 1 || strs[0].length() == 0) return strs[0];
+
+        for (int j = 0; j < strs[0].length(); j++) {
+            for (int i = 1; i < strs.length; i++) {
+                if (strs[i].length() <= j || strs[i].charAt(j) != strs[0].charAt(j)) return strs[0].substring(0, j);
+            }
+        }
+        return strs[0];
+    }
+}
