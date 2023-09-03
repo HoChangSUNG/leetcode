22ce66f59787eb2661e0d58c288a52058c4050dc
Ho <love47024702@naver.com>
2023-09-03 10:56:06 +0900

    9/3

---
+++ src/_9월/_1일_10일/_3일/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,19 @@
+package _9월._1일_10일._3일;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RemoveDuplicatesFromSortedArray {
+
+    public int removeDuplicates(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int idx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.contains(nums[i])) {
+                nums[idx++] = nums[i];
+                set.add(nums[i]);
+            }
+        }
+        return idx;
+    }
+}
---
+++ src/_9월/_1일_10일/_3일/RemoveElement.java
@@ -0,0 +1,14 @@
+package _9월._1일_10일._3일;
+
+public class RemoveElement {
+
+    public int removeElement(int[] nums, int val) {
+        int idx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[idx++] = nums[i];
+            }
+        }
+        return idx;
+    }
+}
