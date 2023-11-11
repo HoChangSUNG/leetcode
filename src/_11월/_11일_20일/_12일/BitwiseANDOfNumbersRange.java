84032de5c4430c9be6472c871179b929ee370a41
HO <love47024702@naver.com>
2023-11-12 00:30:17 +0900

    11/12

---
+++ src/_11월/_11일_20일/_12일/BitwiseANDOfNumbersRange.java
@@ -0,0 +1,14 @@
+package _11월._11일_20일._12일;
+
+public class BitwiseANDOfNumbersRange {
+
+    public int rangeBitwiseAnd(int left, int right) {
+        int shift = 0;
+        while (left < right) {
+            left >>= 1;
+            right >>= 1;
+            shift++;
+        }
+        return left << shift;
+    }
+}
