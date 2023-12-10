d5c6663d6d08f0a898279cf6dbdf6f0b70c8a540
HO <love47024702@naver.com>
2023-12-10 16:11:47 +0900

    12/10

---
+++ src/_12월/_1일_10일/_10일/MinStack.java
@@ -0,0 +1,37 @@
+package _12월._1일_10일._10일;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MinStack {
+    private Deque<Integer> queue;
+    private Deque<Integer> elements;
+
+    public MinStack() {
+        queue = new LinkedList<>();
+        elements = new LinkedList<>();
+    }
+
+    public void push(int val) {
+        int minVal = val;
+        if (!queue.isEmpty()) {
+            minVal = Math.min(minVal, queue.peekLast());
+        }
+        queue.addLast(minVal);
+        elements.addLast(val);
+
+    }
+
+    public void pop() {
+        queue.removeLast();
+        elements.removeLast();
+    }
+
+    public int top() {
+        return elements.peekLast();
+    }
+
+    public int getMin() {
+        return queue.peekLast();
+    }
+}
---
+++ src/_12월/_1일_10일/_10일/SimplifyPath.java
@@ -0,0 +1,25 @@
+package _12월._1일_10일._10일;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class SimplifyPath {
+    public String simplifyPath(String path) {
+        String[] split = path.split("/");
+        Deque<String> deque = new LinkedList<>();
+        for (int i = 0; i < split.length; i++) {
+            String cur = split[i];
+            if (cur.isEmpty() || cur.equals(".")) continue;
+            else if (cur.equals("..")) {
+                if (!deque.isEmpty()) deque.removeLast();
+            } else deque.addLast(cur);
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!deque.isEmpty()) {
+            sb.append("/");
+            sb.append(deque.removeFirst());
+        }
+        if (sb.length() == 0) return "/";
+        return sb.toString();
+    }
+}
