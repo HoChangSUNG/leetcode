1e003fc3f1e034d1f509b10613de0c39f63b2137
HO <love47024702@naver.com>
2023-12-13 13:41:10 +0900

    12/13

---
+++ src/_12월/_11일_20일/_13일/HIndex.java
@@ -0,0 +1,11 @@
+package _12월._11일_20일._13일;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class HIndex {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        return 1;
+    }
+}
