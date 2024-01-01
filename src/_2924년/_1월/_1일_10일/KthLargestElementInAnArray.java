fdf750c01313a38bedd79e34db0349e38766c700
HO <love47024702@naver.com>
2024-01-01 13:54:13 +0900

    1/1

---
+++ src/_2924년/_1월/_1일_10일/KthLargestElementInAnArray.java
@@ -0,0 +1,18 @@
+package _2924년._1월._1일_10일;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class KthLargestElementInAnArray {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> largest = new PriorityQueue(Collections.reverseOrder());
+        for (int num : nums) {
+            largest.add(num);
+        }
+        int result = 0;
+        for (int i = 0; i < k; i++) {
+            result = largest.poll();
+        }
+        return result;
+    }
+}
---
+++ src/_2924년/_1월/_1일_10일/SingleNumberII.java
@@ -0,0 +1,17 @@
+package _2924년._1월._1일_10일;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SingleNumberII {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) return entry.getKey();
+        }
+        return -1;
+    }
+}
