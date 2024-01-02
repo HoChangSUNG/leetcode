9065d46e71bb41da6c51cc9ea218cb51b62096ff
HO <love47024702@naver.com>
2024-01-02 14:26:02 +0900

    1/2

---
+++ src/_2924년/_1월/_1일_10일/_2일/BitwiseAndOfNumbersRange.java
@@ -0,0 +1,13 @@
+package _2924년._1월._1일_10일._2일;
+
+public class BitwiseAndOfNumbersRange {
+    public int rangeBitwiseAnd(int left, int right) {
+        int shift = 0;
+        while (left != right) {
+            left >>= 1;
+            right >>= 1;
+            shift++;
+        }
+        return left << shift;
+    }
+}
---
+++ src/_2924년/_1월/_1일_10일/_2일/FactorialTrailingZeroes.java
@@ -0,0 +1,15 @@
+package _2924년._1월._1일_10일._2일;
+
+public class FactorialTrailingZeroes {
+    public int trailingZeroes(int n) {
+        int result = 0;
+        for (int i = 5; i <= n; i += 5) {
+            int cur = i;
+            while (cur % 5 == 0) {
+                result++;
+                cur /= 5;
+            }
+        }
+        return result;
+    }
+}
