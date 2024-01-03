703fc53cdc62ab174b8f0dc4932d607b41f16cec
HO <love47024702@naver.com>
2024-01-03 15:35:06 +0900

    1/3 문제

--- src/_2024년/_1월/_1일_10일/_3일/FindPeakElement.java
+++ src/_2024년/_1월/_1일_10일/_3일/FindPeakElement.java
@@ -1,2 +1,10 @@
-package _2024년._1월._1일_10일._3일;public class FindPeakElement {
+package _2024년._1월._1일_10일._3일;
+
+public class FindPeakElement {
+    public int findPeakElement(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) return i - 1;
+        }
+        return nums.length - 1;
+    }
 }
