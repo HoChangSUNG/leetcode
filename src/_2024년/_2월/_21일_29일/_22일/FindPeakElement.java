e7930b1a96d2291b880adb226477a7191ceaa453
HO <love47024702@naver.com>
2024-02-22 10:24:26 +0900

    2/22

---
+++ src/_2024년/_2월/_21일_29일/_22일/FindPeakElement.java
@@ -0,0 +1,12 @@
+package _2024년._2월._21일_29일._22일;
+
+public class FindPeakElement {
+    public int findPeakElement(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                return i;
+            }
+        }
+        return nums.length - 1;
+    }
+}
