0435f4138a97dec77bdbf1f99c0e0cd8c31d3a29
HO <love47024702@naver.com>
2024-03-05 15:22:40 +0900

    수정

--- src/_2024년/_3월/_1일_10일/_5일/Pow.java
+++ src/_2024년/_3월/_1일_10일/_5일/Pow.java
@@ -8,7 +8,6 @@ public class Pow {
     private double pow(double x, long n) {
         if (n < 0) return 1 / pow(x, -n);
         else if (n == 0) return 1;
-
         if (n % 2 == 0) return pow(x * x, n / 2);
         else return x * pow(x * x, (n - 1) / 2);
     }
