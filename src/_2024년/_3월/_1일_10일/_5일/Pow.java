53fa044f58e2f26bc04f949536da3103c913a5f3
HO <love47024702@naver.com>
2024-03-05 15:16:43 +0900

    3/5

---
+++ src/_2024년/_3월/_1일_10일/_5일/Pow.java
@@ -0,0 +1,15 @@
+package _2024년._3월._1일_10일._5일;
+
+public class Pow {
+    public double myPow(double x, int n) {
+        return pow(x, (long) n);
+    }
+
+    private double pow(double x, long n) {
+        if (n < 0) return 1 / pow(x, -n);
+        else if (n == 0) return 1;
+
+        if (n % 2 == 0) return pow(x * x, n / 2);
+        else return x * pow(x * x, (n - 1) / 2);
+    }
+}
