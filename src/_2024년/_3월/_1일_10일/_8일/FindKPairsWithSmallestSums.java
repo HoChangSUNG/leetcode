732a259ee0879108c12b7e5a922662171f04bd38
HO <love47024702@naver.com>
2024-03-09 17:54:39 +0900

    3/9

--- src/_2024년/_3월/_1일_10일/_8일/FindKPairsWithSmallestSums.java
+++ src/_2024년/_3월/_1일_10일/_8일/FindKPairsWithSmallestSums.java
@@ -7,19 +7,28 @@ import java.util.PriorityQueue;
 
 public class FindKPairsWithSmallestSums {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<List<Integer>> queue = new PriorityQueue<List<Integer>>((o1, o2) -> {
-            int firsst = o1.get(0)+o1.get(1);
-            int second = o2.get(0)+o2.get(1);
-            return firsst - second;
-        });
-        for(int i = 0;i<nums1.length;i++){
-            for(int j = 0;j<nums2.length;j++){
-                ArrayList<Integer> arr = new ArrayList<>();
-                arr.add(nums1[i]);
-                arr.add(nums2[i]);
-                queue.add(arr);
-            }
+        List<List<Integer>> result = new ArrayList<>();
+        PriorityQueue<Integer[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[0]));
+
+        for (int num1 : nums1) {
+            pq.add(new Integer[]{num1 + nums2[0], 0});
         }
 
+        while (k > 0 && !pq.isEmpty()) {
+            Integer[] cur = pq.poll();
+            int num2Index = cur[1];
+
+            ArrayList<Integer> curPair = new ArrayList<>();
+            curPair.add(cur[0] - nums2[num2Index]);
+            curPair.add(nums2[num2Index]);
+
+            result.add(curPair);
+
+            if (num2Index + 1 < nums2.length) {
+                pq.add(new Integer[]{cur[0] - nums2[num2Index] + nums2[num2Index + 1], num2Index + 1});
+            }
+            k--;
+        }
+        return result;
     }
 }
---
+++ src/_2024년/_3월/_1일_10일/_9일/MaximumSumCircularSubarray.java
@@ -0,0 +1,15 @@
+package _2024년._3월._1일_10일._9일;
+
+public class MaximumSumCircularSubarray {
+    public int maxSubarraySumCircular(int[] A) {
+        int total = 0, maxSum = A[0], curMax = 0, minSum = A[0], curMin = 0;
+        for (int a : A) {
+            curMax = Math.max(curMax + a, a);
+            maxSum = Math.max(maxSum, curMax);
+            curMin = Math.min(curMin + a, a);
+            minSum = Math.min(minSum, curMin);
+            total += a;
+        }
+        return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
+    }
+}
